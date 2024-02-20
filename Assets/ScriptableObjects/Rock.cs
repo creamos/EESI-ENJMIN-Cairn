8ac4ee74a8818d3b74457ea203da7e807a7acd6e
ChicheKebab <118912423+ChicheKebab@users.noreply.github.com>
2024-02-20 17:06:20 +0100

    algo de remplissage du cern fini mais pas testé

--- Assets/ScriptableObjects/Rock.cs
+++ Assets/ScriptableObjects/Rock.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class Rock : ScriptableObject
 {
     public string prefabName;
-    public int rockWidth;
+    public int rockWidth; // de 1 à 4
     // Ajouter l'image, et ce dont a besoin viktor pour les anim
 }
--- Assets/Scripts/CairnManagement.cs
+++ Assets/Scripts/CairnManagement.cs
@@ -4,30 +4,66 @@ using UnityEngine;
 
 public class CairnManagement : MonoBehaviour
 {
-    // Ce serait des scriptable object en fait et pas des GameObjects
     private List<Rock> rocksInCairn = new List<Rock>();
     private List<bool> spaces;
+    private List<int[]> rocksInCairnCoords;
 
-    public int LowestRowWidth;
+    public int lowestFloorWidth;
 
-    
+    void Fit(int idxRockStart, Rock rock , int floor, int idxRockPosInFloor){
+        for (int i =0; i<rock.rockWidth;i++){
+            spaces[idxRockStart+i]= true;
+        }
+        int[] rockCoords = new int[2] {floor+idxRockPosInFloor,floor};
+        rocksInCairnCoords.Add(rockCoords);
+    }
 
     void FillCairn(){ // In progress, jprend mon temps mdr
         int w;
+        int idxFloorStart;
+        int floorWidth ;
+        bool canFit;
+        bool fitted;
+
         foreach (Rock rock in rocksInCairn){
             w = rock.rockWidth;
+            fitted = false;
 
             // Pour chaque rock je parcours tous les etages du cairn pour trouver une place au caillou
-            for (int i=0; i< (LowestRowWidth+1) /2; i++){
+            idxFloorStart=0;
+            floorWidth= lowestFloorWidth;
+            for (int i=0; i< (lowestFloorWidth+1) /2; i++){ //pour chaque etage du cern
+                if(fitted){
+                    break;
+                }
 
+                for(int k=0; k<floorWidth;k++){ // a chaque position de cet etage 
+                    if(k+w <= floorWidth){ //Si le caillou peut rentrer à cet etage depuis cette position, je regarde si l'espace est libre ou deja pris par d'autre caillou 
+                        canFit= true;
+                        for (int j =0; j<w;j++){
+                            if(spaces[idxFloorStart+k+j]==false){
+                                canFit =false;
+                                break;
+                            }
+                        }
+                        if(canFit){ // auquel cas je place le caillou et je sors du process pour passer au cailloux d'apres 
+                            Fit(idxFloorStart+k, rock, i,k);
+                            fitted= true;
+                            break;
+                        }
+                    }
+                }
+
+                idxFloorStart+= floorWidth;
+                floorWidth-=2;
             }
         }
     }
 
     // fonction qui crée un cairn vide ( liste de booleen where true = espace vide)
     void InitializeCairnSpace(){
-        int n = LowestRowWidth+1 ;
-        for( int i=0; i< (LowestRowWidth+1) /2; i++ ){ // pour chaque etage du cairn
+        int n = lowestFloorWidth+1 ;
+        for( int i=0; i< (lowestFloorWidth+1) /2; i++ ){ // pour chaque etage du cairn
             for( int j=0; j< n; j++ ){
                 spaces.Add(true);
             }
