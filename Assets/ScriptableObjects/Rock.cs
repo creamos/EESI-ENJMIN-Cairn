5edcc73695c34e4b7ebf983754509d25a40b96e1
VictorCouturieux <victor.couturieux@laposte.net>
2024-02-20 17:28:37 +0100

    Button with 6fps animation on it

--- Assets/ScriptableObjects/Rock.cs
+++ Assets/ScriptableObjects/Rock.cs
@@ -6,5 +6,7 @@ public class Rock : ScriptableObject
 {
     public string prefabName;
     public int rockWidth; // de 1 à 4
+    
     // Ajouter l'image, et ce dont a besoin viktor pour les anim
+    public Animator animationControl;
 }
