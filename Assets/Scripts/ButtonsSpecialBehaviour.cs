de3845ffba4dc89269c3e66e9225ed32d7618a5c
Lamaxelle <axelledesquilbet.pro@gmail.com>
2024-02-20 15:39:26 +0100

    feat : text mesh pro + menu termé

--- Assets/Scripts/ButtonsSpecialBehaviour.cs
+++ Assets/Scripts/ButtonsSpecialBehaviour.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonsSpecialBehaviour : MonoBehaviour
 {
+    private Scrollbar currentScrollbar;
+    public GameObject[] stonesUI;
+    public GameObject stonePannel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        stonesUI = GameObject.FindGameObjectsWithTag("UIStone");
+        stonePannel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -16,8 +22,24 @@ public class ButtonsSpecialBehaviour : MonoBehaviour
         
     }
 
-    void ToggleObj ()
+    public void SetScrollbar (Scrollbar _scrollbar) 
     {
-    
+        currentScrollbar = _scrollbar;
+    }
+
+    public void ScrollObj (GameObject _scrollingObj)
+    {
+        float _x = _scrollingObj.transform.localPosition.x;
+        float _z = _scrollingObj.transform.localPosition.z;
+        float _objPosY = currentScrollbar.value * 1500;
+        _scrollingObj.transform.localPosition = new Vector3(_x,_objPosY,_z);
+    }
+
+    public void UnableOutline ()
+    {
+        foreach (GameObject outline in stonesUI) 
+        {
+            outline.GetComponent<Outline>().enabled = false;
+        }
     }
 }
