1e473742b598fd499318579665c7287ad6fb6b87
Alex <alexandrelacroix.35@gmail.com>
2024-02-21 16:29:06 +0100

    create faux fix component

---
+++ Assets/Scripts/UI/FauxFix.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class FauxFix : MonoBehaviour
+{
+    private Image image;
+    
+    public Sprite[] Frames;
+    [SerializeField] private float fps = 6;
+
+    private int frameID;
+    private float lastChangeTime;
+    
+    private bool isPlaying;
+    public bool IsPlaying { 
+        get => isPlaying;
+        set
+        {
+            isPlaying = value;
+            if (isPlaying)
+            {
+                lastChangeTime = Time.time;
+                frameID = Math.Max(frameID, 0);
+            }
+        }
+    }
+
+    public void Initialize(Rock data)
+    {
+        Initialize(data.FrameList);
+    }
+    
+    public void Initialize(Sprite[] frames)
+    {
+        Frames = frames;
+        if (frames.Length > 0)
+        {
+            image.sprite = frames[0];
+            frameID = Math.Clamp(frameID, 0, frames.Length);
+        }
+    }
+
+    private void OnEnable()
+    {
+        image ??= GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        if (isPlaying && (Time.time - lastChangeTime) > 1f/fps)
+        {
+            if (Frames.Length == 0) return;
+            frameID = (frameID+1)%Frames.Length;
+            image.sprite = Frames[frameID];
+            lastChangeTime = Time.time;
+        }
+    }
+}
